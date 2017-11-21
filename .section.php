159fb45c8736a860d2920216369d80e4b47cc016
Boris <nomadirl@gmail.com>
2017-11-21 04:59:50 +0300

    git-repository creation

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "Мы предлагаем ассортимент.",
+   "keywords" => "женская одежда, мужская одежда, спортивная одежда, тапочки",
+   "robots" => "index, follow"
+);
+?>
